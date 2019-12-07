3e78cd76d66c80c6482f93b1d57a145335d7bc72
Bryan Geil <beg692@gmail.com>
2019-12-07 14:01:53 -0600

    current user can add families

--- src/ChrisListsContainer/index.js
+++ src/ChrisListsContainer/index.js
@@ -1,12 +1,171 @@
-import React from "react";
+import React, { Component } from "react";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+import CreateFamily from  "../CreateFamily"
 
-function ChrisListsContainer() {
-			return (
-				<div>
-					This is the Chris Lists Container
-				</div>
+class ChrisListsContainer extends Component {
+	constructor() {
+		super();
+		this.state = {
+			families: [],
+			family_members: [],
+			createFamilyModalOpen: false,
+			createFamily: {
+				family_name: ""
+			}
+		}
+	}
+	componentDidMount() {
+		this.getFamilies();
+	}
+
+	// Grabs all the families the current logged in user is realted to.
+	getFamilies = async () => {
+		try {
+			const families = await fetch(
+				process.env.REACT_APP_API_URL + "/api/v1/families/",
+				{
+					credentials: "include"
+				}
 			);
+			const parsedFamilies = await families.json();
+			console.log(parsedFamilies);
+			this.setState({
+				families: parsedFamilies.data
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	// Creates A Fmaily and adds the current user to the family
+	addFamily = async (e) => {
+		e.preventDefault();
+		console.log(this.state.createFamily);
+		try {
+			const createdFamilyResponse = await fetch(
+				process.env.REACT_APP_API_URL + "/api/v1/families/",
+				{
+					method: "POST",
+					credentials: "include",
+					body: JSON.stringify(this.state.createFamily),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				}
+			);
+			const parsedResponse = await createdFamilyResponse.json();
+			console.log(parsedResponse, "this is the response");
+			this.setState({
+				families: [
+				...this.state.families, 
+				parsedResponse.data
+				]
+			});
+			this.closeModal();
+		} 
+		catch (err) {
+			console.log(err);
+		}
+	};
+	handleCreateChange = (e) => {
+		e.preventDefault();
+		this.setState({
+			createFamily: {
+				...this.state.createFamily,
+				[e.target.name]: e.target.value
+			}
+		})
+	}
+
+
+	// Opens Add Family Model
+	createFamilyModalOpen = () => {
+		console.log("hitting the createFamilyModalOpen");
+		this.setState({
+			createFamilyModalOpen: true
+		});
+
+	};
+
+	// Closes All Models 
+	closeModal = () => {
+		this.setState({
+			createFamilyModalOpen: false
+		});
+	};
+
+
+
+
+	render() {
+		// I need to display my nav bar (react router) and 
+		// I need to display the current users families they are related to and
+		// I need to display their family members under each family
+		return (
+		<div>
+			This is the Chris Lists Container
+			<CreateFamily
+				open={this.state.createFamilyModalOpen}
+				addFamily={this.addFamily}
+				handleCreateChange={this.handleCreateChange}
+				closeModal={this.closeModal}
+			/>
+			<button onClick={this.createFamilyModalOpen}>Add a Family</button>
+
+		</div>
+		);
+	}
 		
 }
 
 export default ChrisListsContainer
+
+
+// function Home() {
+// 	  return <h2>Home</h2>;
+// 	}
+
+// 	 <Router>
+// 	      <div>
+// 	        <nav>
+// 	          <ul>
+// 	            <li>
+// 	              <Link to="/">Home</Link>
+// 	            </li>
+// 	          </ul>
+// 	        </nav>
+
+// 	        { A <Switch> looks through its children <Route>s and
+// 	            renders the first one that matches the current URL. }
+// 	        <Switch>
+// 	          <Route path="/about">
+// 	            <About />
+// 	          </Route>
+// 	          <Route path="/users">
+// 	            <Users />
+// 	          </Route>
+// 	          <Route path="/">
+// 	            <Home />
+// 	          </Route>
+// 	        </Switch>
+// 	      </div>
+// 	    </Router>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/CreateFamily/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input } from 'reactstrap';
+
+function CreateFamily(props) {
+
+	return (
+		 <div>
+		      <Modal isOpen={props.open} onClose={props.closeModal}>
+		      <Form onSubmit={props.addFamily}>
+			        <ModalHeader>Add a New Family</ModalHeader>
+			        <ModalBody>
+			        	 <FormGroup>
+					        <Label for="family_name">Add a Family to Chris List!</Label>
+					        <Input type="text" name="family_name" id="family_name" value={props.family_name}
+				                onChange={props.handleCreateChange}/>
+					      </FormGroup>
+
+			        </ModalBody>
+			        <ModalFooter>
+			          <Button color="secondary" type='Submit'>Add a Family</Button>
+			        </ModalFooter>
+		        </Form>
+		      </Modal>
+	    </div>
+		)
+}
+export default CreateFamily
