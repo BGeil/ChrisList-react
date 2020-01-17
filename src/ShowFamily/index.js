371e69ba7c8bca99f4cbe1c769dac79bdc8597fe
Bryan Geil <beg692@gmail.com>
2020-01-16 18:06:37 -0600

    cleared up temp styling

--- src/ShowFamily/index.js
+++ src/ShowFamily/index.js
@@ -8,13 +8,13 @@ function ShowFamily (props) {
 
 		if(props.family_members[0] !== 0) {
 			currentFamily.push(		
-		      	<ListGroup className="list-group-show-family">     
-			        <ListGroupItem tag="a">
+		      	<ListGroup className="show-family-style">     
+			        <ListGroupItem  className="show-family-style" tag="a">
 			        	{props.family_members[0].map((family_member, i) => {
 			        		return (
 		        			<div className="show-family-member" key={i}><h4>{family_member.user_id.first_name.toUpperCase()}</h4>
 		        				{props.family_members[1][i].map(present => <p key={present.id}>{present.present_name}</p>)}
-		        				<hr/>
+		        				
 			        		</div>
 			        	)})}
 			        </ListGroupItem>
@@ -23,7 +23,7 @@ function ShowFamily (props) {
     	}			
 	return(
 		<React.Fragment>
-			<h1 className="family-title">{props.family_members[0][0].family_id.family_name}</h1>
+			<h3 className="family-title">{props.family_members[0][0].family_id.family_name}</h3>
 			{currentFamily}
 		</React.Fragment>
 	)
